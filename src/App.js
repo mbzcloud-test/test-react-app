a99df9258c40944d4aea53082940d633c6996084
TrevorNathan <trevoratwijukire@gmail.com>
2022-10-10 16:49:05 +0300

    UPDATE: v1.1.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import sum, { greetings } from "@mbzcloud-test/mbzcloud-web-comp";
+import multiply from "@mbzcloud-test/mbzcloud-web-comp";
+
+// use the latest github package
+//greetings();
+console.log(greetings);
+
+console.log(sum(2, 2));
+console.log(multiply(2, 2));
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<img src={logo} className='App-logo' alt='logo' />
+				<p>
+					Hello World! from React and mbzcloud-web-comp Github latest package.
+				</p>
+			</header>
+		</div>
+	);
 }
 
 export default App;
